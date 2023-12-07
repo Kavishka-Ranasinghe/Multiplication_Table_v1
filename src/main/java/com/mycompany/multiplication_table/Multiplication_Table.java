04a38d3295c08406b7a8905aa3a2f7c3ed250ede
Kavishka Ranasinghe <kavishka.r.2003@gmail.com>
2023-12-07 22:31:08 +0530

    2nd commit - add simple thank you

--- src/main/java/com/mycompany/multiplication_table/Multiplication_Table.java
+++ src/main/java/com/mycompany/multiplication_table/Multiplication_Table.java
@@ -25,6 +25,7 @@ public class Multiplication_Table {
             System.out.println(""+mul+" x "+i+" = "+(mul*i));
         
         }
+        System.out.println("Thank you for using the service");
         
        
         
